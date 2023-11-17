bcf31e1d582e519368137aeb0db9feb211c04c89
VitorB2002 <vitorb.santos2002@gmail.com>
2023-11-17 16:01:09 -0300

    Add(script.js): adicionado visualização dos detalhes no front

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let estados = document.getElementsByClassName("estado")
+let results = document.getElementById("results")
 let listaApi
 const ids = [
     "BRA1294", 
@@ -42,11 +43,23 @@ function getDetalhes(id) {
     fetch("https://api-estados-br.onrender.com/estados/detalhes/" + id)
     .then((res) => res.json())
     .then(payload => {
+        let resultsData = null
+
         if(payload.status == 200){
-            console.log(payload.data)
+            resultsData = "<h3>Dados do Estado</h3><p>"
+            resultsData += "Estado: " + payload.data.estado + " | UF: (" + payload.data.uf + ")<br>"
+            resultsData += "Região: " + payload.data.regiao + "<br>"
+            resultsData += "Capital: " + payload.data.capital + "<br>"
+            resultsData += "Área (km²): " + payload.data["area_km2"] + "<br>"
+            resultsData += "População: " + payload.data.populacao + "<br>"
+            resultsData += "IDH: " + payload.data.idh + "<br>"
+            resultsData += "Fuso Horário: " + payload.data["fuso_horario"]
+            resultsData += "</p>"
         }   else{
-            console.log("Error")
+            resultsData = "Erro ao chamar a API"
         }
+
+        results.innerHTML = resultsData
     })
 }
 
@@ -54,6 +67,11 @@ getEstados()
 
 for(let i = 0; i < 27; i++){
     estados[ids[i]].addEventListener('click', () => {
+        results.innerHTML = null
+        let searching = document.createElement("img")
+        searching.setAttribute("src", "search.gif")
+        searching.setAttribute("width", "50%")
+        results.append(searching)
         let nomeEstado = estados[ids[i]].getAttribute("name")
         for(let i = 1; i <= 27; i++){
             if(nomeEstado == listaApi[i].estado){
